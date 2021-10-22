2f03b86307735d1895f76e625f69e70c326c9637
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2021-10-23 01:19:28 +0400

    Сванчик ленивый baka

--- lua/plib/cl/vgui/plib_notify.lua
+++ lua/plib/cl/vgui/plib_notify.lua
@@ -1,7 +1,34 @@
-PANEL = {}
+PLib["Notifications"] = PLib["Notifications"] or {}
+
+local function ScreenInit()
+
+end
+
+hook.Add("OnScreenSizeChanged", "PLib:Notify", ScreenInit)
+ScreenInit()
+
+local PANEL = {}
+function PANEL:Setup(title, text, color, time)
+
+end
 
 function PANEL:Init()
+    self:Setup()
+end
+
+function PANEL:PerformLayout()
+
+end
+
+function PANEL:Paint(w, h)
 
 end
 
 vgui.Register("plib_notify", PANEL)
+
+function PLib:AddNotify(title, text)
+    local notify = vgui.Create("plib_notify")
+    notify:Setup(title, text)
+end
+
+-- PLib:AddNotify(title, text)
